b1073ec8df674ec482f73a53d2939cf6e2e84b7c
Etienne Tremel <etienne@kogusenn.com>
2019-07-13 10:30:14 +0200

    Add exec-cronjob

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
 Kubectl plugins
 ===============
 
-> Kubectl plugins repository.
+> Kubectl plugins repository which contains a few helpers that improve the
+kubectl experience.
+
+## Available plugins
 
 |  plugin      | description |
 |--------------|-------------|
 | prune-unused | Prune secrets or configmaps that are not being used in a given namespace. It checks against all resources from mounted volumes, env, envFrom and imagePullSecrets.
+| exec-cronjob | Execute a CronJob, by extracting the Job spec and apply it in the given namespace.
 
 ## Getting started
 
 Install [krew](https://krew.dev) to manage Kubectl plugins. Refer to the
 [Krew documentation](https://krew.dev) to get started.
 
+### Prune unused
+
 ```bash
 # install the prune-used plugins
 $ kubectl krew install prune-unused
 ```
 
-```bash
+```
 Prune unused configmaps/secret resources from a given namespace. It
 checks against all resources from mounted volumes, env and envFrom and
 imagePullSecrets.
@@ -27,5 +33,24 @@ Usage:
 
 Options:
     -n, --namespace='': If present, the namespace scope for this CLI request
-    -h, --help='': Display this help
+    -h, --help: Display this help
+```
+### Exec cronjob
+
+```bash
+# install the exec-cronjob plugins
+$ kubectl krew install exec-cronjob
+```
+
+```
+Execute a CronJob, by extracting the Job spec and apply it in the given
+namespace.
+
+Usage:
+    kubectl exec-cronjob <name> [options]
+
+Options:
+    -n, --namespace='': If present, the namespace scope for this CLI request
+    --dry-run: If true, only print the object that would be sent, without sending it.
+    -h, --help: Display this help
 ```
---
+++ exec-cronjob/kubectl-exec_cronjob.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# Execute a CronJob, by extracting the Job spec and apply it in the given
+# namespace
+
+set -e
+
+function usage() {
+  echo "Execute a CronJob, by extracting the Job spec and apply it in the given"
+  echo "namespace."
+  echo ""
+  echo "Usage:"
+  echo "    kubectl exec-cronjob <name> [options]"
+  echo ""
+  echo "Options:"
+  echo "    -n, --namespace='': If present, the namespace scope for this CLI request"
+  echo "    --dry-run: If true, only print the object that would be sent, without sending it."
+  echo "    -h, --help: Display this help"
+  exit 0
+}
+
+if [ $# -lt 1 ]
+then
+  usage
+fi
+
+cronjob_name=$1
+dry_run_arg=""
+namespace_arg=""
+
+while test $# -gt 1
+do
+  case "$2" in
+    -n|--namespace*)
+      if [[ $2 == *"="* ]]
+      then
+        namespace_arg=${2#=*}
+      else
+        shift
+        namespace_arg="--namespace=${2}"
+      fi
+      shift
+      ;;
+    --dry-run)
+      dry_run_arg="--dry-run -o yaml"
+      shift
+      ;;
+    -h|--help)
+      usage
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+cron_job=$(kubectl get cronjob $namespace_arg -o yaml $cronjob_name)
+
+api_version=$(echo $cron_job | awk '/^apiVersion: / {print $2}')
+
+job="apiVersion: batch/v1
+kind: Job"
+
+# extract the cronjob spec and insert the job name
+job_spec=$(echo "$cron_job" | awk -v name=$cronjob_name '
+  capture && !/^    / { capture=0 }
+  capture { sub("^    ", ""); print $0 }
+  capture && /metadata:/ && !named { print "  name: " name; named=1 }
+  /jobTemplate:$/ { capture=1 }
+')
+
+echo "${job}
+${job_spec}" | kubectl apply -f - $dry_run_arg $namespace_arg
